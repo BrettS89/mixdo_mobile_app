c624bd91f3c3bc6c0371c15b0af819498df77f25
BrettS89 <blsodie@gmail.com>
2018-11-24 16:18:31 -0500

    creating new todos component

---
+++ src/components/publicTodos/PublicTodos.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+class PublicTodos extends React.Compnent {
+  render() {
+    return (
+      <View>
+      </View>
+    );
+  }
+}
+
+export default PublicTodos;
